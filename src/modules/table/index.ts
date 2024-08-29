63507d1559af0a742933cad7b184ad44a24fb286
Андрей Сельдюков <a.seldukov@onpoint.ru>
2024-08-29 18:31:46 +0300

    tc-header

--- src/modules/table/index.ts
+++ src/modules/table/index.ts
@@ -1,16 +1,19 @@
-import {Props, TableBlock} from '../interfaces';
-import { baseBlock } from '../utils';
+import { Props, TableBlock } from "../interfaces";
+import { baseBlock } from "../utils";
 
-export function table ({ data }: Props<TableBlock>) {
+export function table({ data }: Props<TableBlock>) {
   const item = `<div class="cdx-block tc-wrap tc-wrap--readonly">
-            <div class="tc-table">
-              ${data.content.map((row) => 
-                `<div class="tc-row">${
-                  row.map((cell) => 
-                    `<div class="tc-cell">${cell}</div>`
-                  ).join('')
-                }</div>`
-  ).join('')}
+            <div class="tc-table ${
+              data.withHeadings ? "tc-table--heading" : ""
+            }">
+              ${data.content
+                .map(
+                  (row) =>
+                    `<div class="tc-row">${row
+                      .map((cell) => `<div class="tc-cell">${cell}</div>`)
+                      .join("")}</div>`
+                )
+                .join("")}
             </div>
           </div>`;
   return baseBlock(item);
